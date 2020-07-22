82a9ea290e2ea25fbc6f8d2ce1e52ac0f279caf0
David León Galisteo <dadvanced@hotmail.es>
2020-07-22 21:20:50 +0200

    Add files via upload
    add int class

---
+++ variables/MiClaseInt.java
@@ -0,0 +1,34 @@
+package com.mycompany.tutorialesjava.variables;
+
+/**
+ *
+ * @author David
+ */
+public class MiClaseInt {
+    public static void main(String[] args) {
+        
+        int edad;
+        int altura;
+        int kilosComida;
+        
+        edad = 32;
+        altura = 168;
+        kilosComida 
+                = 50;
+        
+        System.out.println("Edad: " + edad + " Altura: " + altura + " kilos: " + kilosComida);
+        
+        int a;
+        int b;
+        int c;
+        
+        a = 2;
+        b = 3;
+        c = 5;
+        int d = a + b;
+        
+        System.out.println(); // esto provoca un salto de línea
+        System.out.println("a + b = " + (a + b));
+        
+    }
+}
